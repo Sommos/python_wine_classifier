4a943ef5cbee7b346c7a157ca926804ab232651c
Sommos <Sommosgaming@gmail.com>
2023-06-15 05:05:46 +0100

    added classifier save and load with compression and decompression

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import pickle
+import bz2
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -80,30 +83,37 @@ print(classification_report(y_test, pred_sgd))
 cross_val = cross_val_score(estimator = sgd, X = x_train, y = y_train, cv = 10)
 print("SGD Accuracy = " + str(cross_val.mean() * 100) + "%")
 
-# hyperparameter tuning for random forest classifier
-# number of trees in random forest
-n_estimators = [int(x) for x in np.linspace(start = 50, stop = 1000, num = 10)]
-# number of features to consider at every split
-max_features = ["sqrt"]
-# maximum number of levels in tree
-max_depth = [int(x) for x in np.linspace(10, 110, num = 11)] + [None]
-# minimum number of samples required to split a node
-min_samples_split = [2, 5, 10]
-# minimum number of samples required at each leaf node
-min_samples_leaf = [1, 2, 4]
-# method of selecting samples for training each tree
-bootstrap = [True, False]
+# check if classifier file exists, and that it is not empty
+if os.path.isfile("classifiers/white_wine_classifier") and (os.path.getsize("classifiers/white_wine_classifier") > 0):
+    # load classifier from file
+    ifile = bz2.BZ2File("classifiers/white_wine_classifier", "rb")
+    rf_optimised = pickle.load(ifile)
+    ifile.close()
+else:
+    # hyperparameter tuning for random forest classifier
+    # number of trees in random forest
+    n_estimators = [int(x) for x in np.linspace(start = 50, stop = 1000, num = 10)]
+    # number of features to consider at every split
+    max_features = ["sqrt"]
+    # maximum number of levels in tree
+    max_depth = [int(x) for x in np.linspace(10, 110, num = 11)] + [None]
+    # minimum number of samples required to split a node
+    min_samples_split = [2, 5, 10]
+    # minimum number of samples required at each leaf node
+    min_samples_leaf = [1, 2, 4]
+    # method of selecting samples for training each tree
+    bootstrap = [True, False]
 
-# create random grid
-random_grid = {"n_estimators": n_estimators,
-                "max_features": max_features,
-                "max_depth": max_depth,
-                "min_samples_split": min_samples_split,
-                "min_samples_leaf": min_samples_leaf,
-                "bootstrap": bootstrap}
+    # create random grid
+    random_grid = {"n_estimators": n_estimators,
+                    "max_features": max_features,
+                    "max_depth": max_depth,
+                    "min_samples_split": min_samples_split,
+                    "min_samples_leaf": min_samples_leaf,
+                    "bootstrap": bootstrap}
 
-# random search of parameters, using 3 fold cross validation, search across 100 different combinations, and use all available cores
-rf_optimised = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose = 2, random_state = 42, n_jobs = -1)
+    # random search of parameters, using 3 fold cross validation, search across 100 different combinations, and use all available cores
+    rf_optimised = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose = 2, random_state = 42, n_jobs = -1)
 
 # fit the random search model
 rf_optimised.fit(x_train, y_train)
@@ -115,3 +125,8 @@ print(rf_optimised.best_params_)
 # random forest classifier with optimised parameters
 rfeval = cross_val_score(estimator = rf_optimised, X = x_train, y = y_train, cv = 10)
 print("Optimised Random Forest Accuracy = " + str(rfeval.mean() * 100) + "%")
+
+# save classifier to file
+ofile = bz2.BZ2File("classifiers/white_wine_classifier", "wb")
+pickle.dump(rf_optimised, ofile)
+ofile.close()
